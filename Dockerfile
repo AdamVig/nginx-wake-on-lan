844c9c8af646fbcb54bf6e6425d2d5f7f0edc86b
Adam Vigneaux <adam@adamvig.com>
2019-05-27 20:15:42 -0400

    Add Go web server

--- Dockerfile
+++ Dockerfile
@@ -2,12 +2,14 @@ FROM golang as builder
 
 WORKDIR /app
 
-COPY . .
+COPY go.mod .
+COPY main.go .
 
 RUN go build -o wake-on-lan
 
 FROM scratch
 
 COPY --from=builder /app/wake-on-lan .
+COPY index.html .
 
 CMD ["./wake-on-lan"]
--- main.go
+++ main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func pageHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "index.html")
+}
+
+func handleMissingParam(w http.ResponseWriter, name string) {
+	w.WriteHeader(http.StatusBadRequest)
+	fmt.Fprintf(w, "missing query parameter '%s'", name)
+}
+
+func wakeHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	if _, ok := r.Form["mac"]; !ok {
+		handleMissingParam(w, "mac")
+	} else if _, ok := r.Form["ip"]; !ok {
+		handleMissingParam(w, "ip")
+	} else {
+		fmt.Fprintf(w, "waking computer with MAC '%s' using broadcast IP '%s'", r.Form.Get("mac"), r.Form.Get("ip"))
+	}
+}
 
 func main() {
-  fmt.Println("hello world")
+	http.HandleFunc("/wake", wakeHandler)
+	http.HandleFunc("/", pageHandler)
+	log.Fatal(http.ListenAndServe(":8000", nil))
 }
